4eb6b1ec24da9a37d813e92fc7319777d8a07a20
Pabloo22 <pablete.arino@gmail.com>
2024-03-15 14:22:49 +0100

    Refactor Trainer class to use DataLoaderHandler

--- captcha_competition/training/trainer.py
+++ captcha_competition/training/trainer.py
@@ -3,61 +3,34 @@ from torch.utils.data import DataLoader
 import wandb
 
 
+from captcha_competition.training import CustomCategoricalCrossEntropyLoss, DataLoaderHandler
+
+
 class Trainer:
 
     def __init__(
         self,
         model: torch.nn.Module,
         optimizer: torch.optim.Optimizer,
-        dataloader: DataLoader,
-        batch_size=64,
-        num_workers=4,
-        num_batches=1000,
-        wandb_project="captcha_recognition",
+        data_loader_handler: DataLoaderHandler,
+        wandb_project=None,
     ):
         self.model = model
         self.optimizer = optimizer
         self.wandb_project = wandb_project
+        self.data_loader_handler = data_loader_handler
 
-    def train(self, num_epochs):
-        wandb.init(
-            project=self.wandb_project,
-            config={
-                "batch_size": self.dataloader.batch_size,
-                "num_epochs": num_epochs,
-                # Include other relevant configurations
-            },
-        )
-
+    def train(self, num_epochs: int):
         for epoch in range(num_epochs):
             self.model.train()
-            for batch_idx, (images, labels) in enumerate(self.dataloader):
+            for batch_idx, (images, labels) in enumerate(self.data_loader_handler):
                 loss = self.training_step(images, labels)
-
-                wandb.log({"train_loss": loss, "batch_idx": batch_idx})
 
     def training_step(self, images, labels):
         self.optimizer.zero_grad()
-        outputs = self.model(
-            images
-        )  # Ensure the model's output matches the expected shape
+        outputs = self.model(images)
         loss_fn = CustomCategoricalCrossEntropyLoss()
         loss = loss_fn(outputs, labels)
         loss.backward()
         self.optimizer.step()
         return loss.item()
-
-    def calculate_loss(self, outputs, labels):
-        # Implement loss calculation based on the specific requirements
-        pass
-
-    # Additional methods for evaluation, accuracy calculation, and model checkpointing can be added here
-
-
-if __name__ == "__main__":
-    # Example initialization and training call
-    # model = None  # Your model initialization
-    # optimizer = None  # Your optimizer initialization
-    # dataset = SyntheticCaptchaIterableDataset()
-    # trainer = Trainer(model, optimizer, dataset)
-    # trainer.train(num_epochs=10)
