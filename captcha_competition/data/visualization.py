3845f90a487e7f3520c86d99ddb77f40bbca42f4
Pabloo22 <pablete.arino@gmail.com>
2024-03-16 14:47:10 +0100

    Refactor plot_image function to accept an optional ax parameter

--- captcha_competition/data/visualization.py
+++ captcha_competition/data/visualization.py
@@ -4,9 +4,14 @@ import cv2
 import numpy as np
 
 
-def plot_image(image_array: np.ndarray, use_cvt_color: bool = True) -> Figure:
+def plot_image(
+    image_array: np.ndarray, use_cvt_color: bool = True, ax=None
+) -> Figure:
     """Plots an image from a numpy array."""
-    plt.figure(figsize=(5, 5))
+    if ax is None:
+        plt.figure(figsize=(5, 5))
+    else:
+        plt.sca(ax)
     new_image_array = image_array
     if use_cvt_color:
         new_image_array = cv2.cvtColor(image_array, cv2.COLOR_BGR2RGB)
