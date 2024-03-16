f015b8010b452c57f58019ed60e92c03f5a119f6
Pabloo22 <pablete.arino@gmail.com>
2024-03-16 14:22:27 +0100

    Refactor import statements in data module

--- captcha_competition/data/__init__.py
+++ captcha_competition/data/__init__.py
@@ -1,16 +1,18 @@
-from .load_data import load_raw_data
-from .visualization import plot_image
-from .generate_captcha import (
+from captcha_competition.data.load_data import load_raw_data
+from captcha_competition.data.visualization import plot_image
+from captcha_competition.data.generate_captcha import (
     generate_captcha_image,
     load_random_font,
     list_all_fonts,
 )
-from .preprocessing import (
+from captcha_competition.data.preprocessing import (
     remove_bg_v1,
     image_to_tensor,
     label_to_tensor,
     generate_captcha_tensors,
     remove_bg_to_tensor,
 )
-from .iterable_dataset import SyntheticCaptchaIterableDataset
-from .dataset import CaptchaDataset
+from captcha_competition.data.iterable_dataset import (
+    SyntheticCaptchaIterableDataset,
+)
+from captcha_competition.data.dataset import CaptchaDataset
