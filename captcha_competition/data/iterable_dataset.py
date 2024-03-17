66841d5cef271045f051ded6cdf1e9a496b4dc5d
Pabloo22 <pablete.arino@gmail.com>
2024-03-17 18:56:24 +0100

    Refactor preprocessing function in SyntheticCaptchaIterableDataset

--- captcha_competition/data/iterable_dataset.py
+++ captcha_competition/data/iterable_dataset.py
@@ -16,9 +16,7 @@ class SyntheticCaptchaIterableDataset(IterableDataset):
 
     def __init__(
         self,
-        preprocessing_fc: Callable[
-            [np.ndarray], torch.Tensor
-        ] = image_to_tensor,
+        preprocessing_fc: Callable[[np.ndarray], np.ndarray],
     ):
         super().__init__()
         self.preprocessing_fc = preprocessing_fc
@@ -29,13 +27,15 @@ class SyntheticCaptchaIterableDataset(IterableDataset):
         # This method generates CAPTCHA images and labels indefinitely
         while True:
             captcha_image, label = generate_captcha_image()
-            captcha_image_tensor = self.preprocessing_fc(captcha_image)
+            captcha_image = self.preprocessing_fc(captcha_image)
+
+            captcha_image_tensor = image_to_tensor(captcha_image)
             label_tensor = label_to_tensor(label)
             yield captcha_image_tensor, label_tensor
 
 
 if __name__ == "__main__":
-    dataset = SyntheticCaptchaIterableDataset()
+    dataset = SyntheticCaptchaIterableDataset(preprocessing_fc=lambda x: x)
     dataloader = DataLoader(dataset, batch_size=64, num_workers=4)
     num_samples = 10
 
