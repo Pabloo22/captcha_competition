38f035f871f1a28819373f9f8f47c0879eb0c555
Pabloo22 <pablete.arino@gmail.com>
2024-03-17 16:21:09 +0100

    Fix import order in training module

--- captcha_competition/training/__init__.py
+++ captcha_competition/training/__init__.py
@@ -1,5 +1,5 @@
 from .loss import CustomCategoricalCrossEntropyLoss
 from .data_loader_handler import DataLoaderHandler
-from .trainer import Trainer
 from .metric import CustomAccuracyMetric, custom_accuracy
+from .trainer import Trainer
 from .factories import trainer_factory
