d72331edec6bd78702534c0dc9212e648df05397
Pabloo22 <pablete.arino@gmail.com>
2024-03-19 21:45:21 +0100

    Add save_processed flag to CaptchaDataset class

--- captcha_competition/data/dataset.py
+++ captcha_competition/data/dataset.py
@@ -27,6 +27,7 @@ class CaptchaDataset(Dataset):
         raw_data_dir: Path = DATA_RAW_PATH,
         processed_data_dir: Path = DATA_PROCESSED_PATH,
         remove_previously_processed: bool = False,
+        save_processed: bool = False,
     ):
         super().__init__()
         self.raw_data_dir = raw_data_dir
@@ -46,6 +47,7 @@ class CaptchaDataset(Dataset):
         )
 
         self.preprocessing_fc = preprocessing_fc
+        self.save_processed = save_processed
 
     def __len__(self):
         return len(self.img_labels)
@@ -58,19 +60,27 @@ class CaptchaDataset(Dataset):
         if not os.path.exists(processed_img_path):
             raw_image = imageio.imread(raw_img_path)
             processed_image = self.preprocessing_fc(raw_image)
-            save_img = (processed_image * 255).astype(np.uint8)
-            imageio.imwrite(processed_img_path, save_img)
+            if self.save_processed:
+                save_img = (processed_image * 255).astype(np.uint8)
+                imageio.imwrite(processed_img_path, save_img)
             image_tensor = image_to_tensor(processed_image)
         else:
-            image_tensor = read_image(processed_img_path)
-            image_tensor = image_tensor.float() / 255.0
-            image_tensor = image_tensor.to(DEVICE)
+            image_tensor = self._read_image_with_torch(processed_img_path)
 
+        label_tensor = self._get_label_tensor(idx)
+        return image_tensor, label_tensor
+
+    @staticmethod
+    def _read_image_with_torch(img_path) -> torch.Tensor:
+        image_tensor = read_image(img_path)
+        image_tensor = image_tensor.float() / 255.0
+        image_tensor = image_tensor.to(DEVICE)
+        return image_tensor
+
+    def _get_label_tensor(self, idx):
         label = self.img_labels.iloc[idx, 1]
         label = [int(i) for i in str(label).zfill(6)]
-
-        label_tensor = label_to_tensor(label)
-        return image_tensor, label_tensor
+        return label_to_tensor(label)
 
 
 if __name__ == "__main__":
