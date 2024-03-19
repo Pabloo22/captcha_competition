a6e94bd7bef0882e88cec43bf6305862c1036e73
Pabloo22 <pablete.arino@gmail.com>
2024-03-20 00:30:04 +0100

    Add factories and functions for dataset, model, optimizer, preprocessing_fc, and get_model_name

--- captcha_competition/training/__init__.py
+++ captcha_competition/training/__init__.py
@@ -2,4 +2,11 @@ from .loss import CustomCategoricalCrossEntropyLoss
 from .data_loader_handler import DataLoaderHandler
 from .metric import CustomAccuracyMetric, custom_accuracy
 from .trainer import Trainer
-from .factories import trainer_factory
+from .factories import (
+    trainer_factory,
+    dataset_factory,
+    model_factory,
+    optimizer_factory,
+    preprocessing_fc_factory,
+    get_model_name,
+)
